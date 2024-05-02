f06eb6568a11a800082a2728f8550c8ef8c72893
Júlia <166644981+julialpsz@users.noreply.github.com>
2024-05-02 16:34:08 -0300

    Create ArrayListString.java

---
+++ ArrayListString.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class ExemploString {
+	public static void main(String[] args){
+		ArrayList<String> nomes = new ArrayList<String>();
+
+		nomes.add("Júlia");
+		nomes.add("Danillo");
+		nomes.add("Ana");
+			
+			System.out.println(nomes);
+		
+			
+		}
+		
+	}  
