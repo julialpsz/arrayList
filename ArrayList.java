fab54e6737471b39cdc96abcd4ffb4cc76c8ac1a
Júlia <166644981+julialpsz@users.noreply.github.com>
2024-05-02 16:17:30 -0300

    Create ArrayList.java

---
+++ ArrayList.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class ExemploArrayList {
+	public static void main(String[] args) {
+		ArrayList<Integer> nomes = new ArrayList<Integer>();
+
+		nomes.add(40028922);
+		nomes.add(488494);
+		nomes.add(9999999); 
+		
+		System.out.println(nomes);
+		
+		
+		
+		
+		
+	}
+	
+}
