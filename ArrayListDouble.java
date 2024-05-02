85203f4bbab6992bc3772ae2c9ed2dfa26edb6ff
Júlia <166644981+julialpsz@users.noreply.github.com>
2024-05-02 16:46:16 -0300

    Create ArrayListDouble.java

---
+++ ArrayListDouble.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class ExemploDouble {
+	public static void main(String[] args){
+		ArrayList<Double> valor = new ArrayList<Double>();	
+		
+		valor.add(12.5);
+		valor.add(3.14159265358979323846);
+		valor.add(686.8877777);
+	
+	System.out.println(valor);
+	
+	}	
+} 
