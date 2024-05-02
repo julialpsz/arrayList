53df27369b3284c6a18211445bd65f11d0f27fc4
Júlia <166644981+julialpsz@users.noreply.github.com>
2024-05-02 16:32:57 -0300

    Create ArrayListInteger.java

---
+++ ArrayListInteger.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class ExemploArrayList {
+	public static void main(String[] args) {
+		ArrayList<Integer> idade = new ArrayList<Integer>();
+
+		idade.add(17);
+		idade.add(18);
+		idade.add(16); 
+		
+		System.out.println(idade);
+			
+		
+	}
+	
+}
